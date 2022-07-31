bbc67a98eb0e04c9d12aa42f248ada6c8b723a31
arkosh <trukan24@gmail.com>
2022-07-31 14:36:46 +0300

    added mu name to mutex

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 type cache struct {
-	sync.RWMutex
+	mu   *sync.RWMutex
 	data map[string]any
 }
 
@@ -15,15 +15,15 @@ func NewCache() *cache {
 }
 
 func (cache *cache) Set(key string, value any) {
-	cache.Lock()
-	defer cache.Unlock()
-
+	cache.mu.Lock()
 	cache.data[key] = value
+	cache.mu.Unlock()
+
 }
 
 func (cache *cache) Get(key string) (any, error) {
-	cache.RLock()
-	defer cache.RUnlock()
+	cache.mu.RLock()
+	defer cache.mu.RUnlock()
 
 	value, ok := cache.data[key]
 	if !ok {
@@ -33,8 +33,8 @@ func (cache *cache) Get(key string) (any, error) {
 }
 
 func (cache *cache) Delete(key string) error {
-	cache.Lock()
-	defer cache.Unlock()
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
 
 	_, ok := cache.data[key]
 	if ok {
